ccf43f5a776ce70098c6ed4fcd2b1c642e46ec5c
JM-arlopes <jmiguelarlopes@gmail.com>
2023-08-03 23:41:38 -0300

    algumas alterações

--- 01-Bateria-JS/js/app.js
+++ 01-Bateria-JS/js/app.js
@@ -1,19 +1,18 @@
-const botoes = document.querySelectorAll('.btn'); 
+const botoes = document.querySelectorAll('.btn');
 
 function tocaSom(dataAddress) {
-    const dataPlay = dataAddress;
 
-    const sound = document.querySelector(`audio[data-sound=${dataPlay}]`)
-    const botao = document.querySelector(`.btn[data-sound=${dataPlay}]`)
-    if(!sound) return;
-    
+    const sound = document.querySelector(`audio[data-sound=${dataAddress}]`)
+    const botao = document.querySelector(`.btn[data-sound=${dataAddress}]`)
+    if (!sound) return;
+
     sound.currentTime = 0;
     sound.play();
-    botao.classList.add('active');    
+    botao.classList.add('active');
 }
 
-function removeTransition (e) {
-    if(e.propertyName !== 'transform') return; // cancela função
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // cancela função
     this.classList.remove('active'); // remove classe de animação
 }
 
@@ -24,31 +23,3 @@ botoes.forEach(btn => {
 })
 
 window.addEventListener('keydown', (e) => tocaSom(e.key.toLowerCase()));
-
-// CÓDIGO ANTIGO: 
-
-// function tocaSomTeclado(e) {
-//     const sound = document.querySelector(`audio[data-sound=${e.key.toLowerCase()}]`) // puxando elemento pressionado
-//     if(!sound) return; // cancela função
-
-//     sound.currentTime = 0;
-//     sound.play(); // toca o audio clicado
-
-//     const botao = document.querySelector(`.btn[data-sound=${e.key.toLowerCase()}]`); // puxa botão pressionado
-//     botao.classList.add('active'); // insere classe de animação
-// }
-
-
-
-// function tocaSomClick(e) {
-//     console.log(e.target.dataset.sound)
-//     const dataPlay = e.target.dataset.sound;
-
-//     const sound = document.querySelector(`audio[data-sound=${dataPlay}]`)
-//     if(!sound) return;
-//     const botao = document.querySelector(`.btn[data-sound=${dataPlay}`)
-
-//     sound.currentTime = 0;
-//     sound.play();
-//     botao.classList.add('active');    
-// }
--- 03-Manipule-Variaveis-CSS/js/app.js
+++ 03-Manipule-Variaveis-CSS/js/app.js
@@ -5,6 +5,7 @@ function handleUpdate() {
     document.documentElement.style.setProperty(`--${this.name}`, this.value + sufflex)
 }
 
-inputs.forEach(input =>  input.addEventListener('change', handleUpdate))
-inputs.forEach(input =>  input.addEventListener('mousemove', handleUpdate))
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+console.log(document.documentElement.style)
 
